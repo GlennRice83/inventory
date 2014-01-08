7cf2421551cb7b4e16a6eaa0b766bebd1ca36478
David Elliott <david@gophilosophie.com>
2014-01-08 15:25:32 -0800

    update spec

--- spec/features/managing_containers_spec.rb
+++ spec/features/managing_containers_spec.rb
@@ -1,22 +1,25 @@
 require "spec_helper"
 
 feature "Managing containers" do
-  let(:parent) { create(:container) }
-  scenario "container be created and inserted into the tree" do
-    visit new_container_path
-
-    fill_in "Name", with: "My Container"
-    fill_in "Parent", with: parent.id
-    click_on "Create"
-
-    expect(page).to have_css("div", text: "container successfully created")
-    expect(Container.where(name: "My Container").count).to eq(1)
-  end
-
   scenario "can be viewed as an index" do
     create_list(:container, 3)
     visit containers_path
 
     expect(page).to have_css("article.container", count: 3)
+  end
+
+  context "creating containers" do
+    let!(:parent) { create(:container) }
+
+    scenario "container be created and inserted into the tree" do
+      visit new_container_path
+
+      fill_in "Name", with: "My Container"
+      select parent.name, from: "Parent"
+      click_on "Create"
+
+      expect(page).to have_css("div", text: "container successfully created")
+      expect(Container.where(name: "My Container").count).to eq(1)
+    end
   end
 end
