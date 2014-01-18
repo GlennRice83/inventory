a5826133e1eeb5de2dd32f1ca6de9a63927535f8
David Elliott <david@gophilosophie.com>
2014-01-17 16:34:46 -0800

    add spec for stock_manager

---
+++ spec/services/stock_manager_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+describe "StockManager", pending: true do
+  it "validates the state of ShoppingList.list_items"
+  it "duplicates each ListItem.item"
+  it "assigns each duplicated Item to Item.food.default_container"
+end
