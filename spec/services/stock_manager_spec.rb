30ece61901fde984e34aeb1af92a17fbbbfd9bcf
David Elliott <david@gophilosophie.com>
2014-01-18 10:05:20 -0800

    add context to #incoming_transfer spec

--- spec/services/stock_manager_spec.rb
+++ spec/services/stock_manager_spec.rb
@@ -1,7 +1,11 @@
 require "spec_helper"
 
 describe "StockManager", pending: true do
-  it "validates the state of ShoppingList.list_items"
-  it "duplicates each ListItem.item"
-  it "assigns each duplicated Item to Item.food.default_container"
+  describe "#incoming_transfer" do
+    it "validates the state of ShoppingList.list_items"
+    it "duplicates each ListItem.item"
+    it "assigns each duplicated Item to Item.food.default_container"
+  end
+
+  describe "#outgoing_transfer"
 end
