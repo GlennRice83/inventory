d6206ac72fdba36f0387e9f6555527e6966dfb8b
David Elliott <david@gophilosophie.com>
2014-08-14 12:36:09 -0700

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Mother::Application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :items
-      resources :containers
       resources :foods
-      resources :recipes
     end
   end
 
